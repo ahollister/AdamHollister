e67daead60ec549b609e202c587f5f6a0ac03cda
Adam Hollister <ah@pragmatic-web.co.uk>
2018-06-26 02:38:23 +0100

    Setting up fields for cv template, adding material buttons and some svg icons

--- header.php
+++ header.php
@@ -23,13 +23,13 @@
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php bloginfo( 'name' ); ?>
+						<?php bloginfo( 'name' ); ?>,
 					</a>
 				</h1>
 			<?php else : ?>
 				<p class="site-title">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php bloginfo( 'name' ); ?>
+						<?php bloginfo( 'name' ); ?>,
 					</a>
 				</p>
 			<?php endif; ?>
